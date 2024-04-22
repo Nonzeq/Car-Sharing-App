396a08e2af8417d92c267b60455a0abab706ac33
Nonzeq <bgdn.kobylchak@gmail.com>
2024-04-22 12:38:42 +0300

    added liquibase scripts for user,roles,user_roles tables

--- src/main/java/com/kobylchak/carsharing/model/User.java
+++ src/main/java/com/kobylchak/carsharing/model/User.java
@@ -32,10 +32,10 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
     @ManyToMany
