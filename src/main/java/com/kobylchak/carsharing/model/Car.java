040c9dd7677b118dc399e612fa7dd2bd3f01b3a4
Nonzeq <bgdn.kobylchak@gmail.com>
2024-04-22 11:12:17 +0300

    added .env.example file

--- src/main/java/com/kobylchak/carsharing/model/Car.java
+++ src/main/java/com/kobylchak/carsharing/model/Car.java
@@ -8,7 +8,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
 import java.math.BigDecimal;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
