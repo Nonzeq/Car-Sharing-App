2d58755c9a3a11c617574e4947f550e3e5170b65
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-04 01:06:39 +0300

    fix check style

--- src/main/java/com/kobylchak/carsharing/service/notification/NotificationService.java
+++ src/main/java/com/kobylchak/carsharing/service/notification/NotificationService.java
@@ -1,7 +1,5 @@
 package com.kobylchak.carsharing.service.notification;
 
-import com.kobylchak.carsharing.model.Rental;
-
 public interface NotificationService<T> {
     void sendNotification(T value);
 }
