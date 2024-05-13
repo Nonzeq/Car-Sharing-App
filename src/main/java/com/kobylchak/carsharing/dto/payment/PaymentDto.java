d5fb04738b80349b3ea9c0905a96e0b30b79967e
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-13 11:36:12 +0300

    modify PaymentMapper

--- src/main/java/com/kobylchak/carsharing/dto/payment/PaymentDto.java
+++ src/main/java/com/kobylchak/carsharing/dto/payment/PaymentDto.java
@@ -1,9 +1,17 @@
 package com.kobylchak.carsharing.dto.payment;
 
+import com.kobylchak.carsharing.model.enums.PaymentStatus;
+import com.kobylchak.carsharing.model.enums.PaymentType;
+import java.math.BigDecimal;
 import java.net.URL;
 import lombok.Data;
 
 @Data
 public class PaymentDto {
+    private Long id;
     private URL sessionUrl;
+    private PaymentStatus status;
+    private PaymentType type;
+    private BigDecimal amountToPay;
+    private Long rentalId;
 }
--- src/main/java/com/kobylchak/carsharing/mapper/payment/PaymentMapper.java
+++ src/main/java/com/kobylchak/carsharing/mapper/payment/PaymentMapper.java
@@ -5,9 +5,11 @@ import com.kobylchak.carsharing.dto.payment.PaymentDto;
 import com.kobylchak.carsharing.model.Payment;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface PaymentMapper {
+    @Mapping(target = "rentalId", source = "payment.rental.id")
     PaymentDto toDto(Payment payment);
     
     List<PaymentDto> toDtos(List<Payment> payments);
