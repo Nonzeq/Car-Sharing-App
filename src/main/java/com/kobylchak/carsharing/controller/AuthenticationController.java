7749a380ce756707b7d06e52331dd975d219e5b1
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-15 17:11:05 +0300

    addded swagger

--- src/main/java/com/kobylchak/carsharing/controller/AuthenticationController.java
+++ src/main/java/com/kobylchak/carsharing/controller/AuthenticationController.java
@@ -6,6 +6,8 @@ import com.kobylchak.carsharing.dto.user.UserRegistrationRequestDto;
 import com.kobylchak.carsharing.dto.user.UserResponseDto;
 import com.kobylchak.carsharing.service.auth.AuthenticationService;
 import com.kobylchak.carsharing.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,21 +20,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(
+        name = "Authentication management",
+        description = "Endpoints for authentication and login users"
+)
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
     private final UserService userService;
     
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Login user")
     public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
         return authenticationService.authenticate(requestDto);
     }
     
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Register user")
     public UserResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.registerUser(requestDto);
     }
-
+    
 }
--- src/main/java/com/kobylchak/carsharing/controller/UserController.java
+++ src/main/java/com/kobylchak/carsharing/controller/UserController.java
@@ -5,8 +5,11 @@ import com.kobylchak.carsharing.dto.user.UpdateUserInfoRequestDto;
 import com.kobylchak.carsharing.dto.user.UserResponseDto;
 import com.kobylchak.carsharing.model.User;
 import com.kobylchak.carsharing.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +17,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(
+        name = "Users management",
+        description = "Endpoints for users management"
+)
 public class UserController {
     private final UserService userService;
     
     @PutMapping("/{userId}/role")
     @PreAuthorize("hasRole('MANAGER')")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update user role by id")
     public void updateUserRole(@PathVariable Long userId,
                                           @RequestBody @Valid UpdateRoleRequestDto requestDto) {
         userService.updateUserRole(userId, requestDto);
@@ -31,6 +41,8 @@ public class UserController {
     
     @GetMapping("/me")
     @PreAuthorize("hasAnyRole('MANAGER', 'CUSTOMER')")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get user user info for the current authorized user")
     public UserResponseDto getCurrentUserInfo(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         return userService.getUserInfo(user);
@@ -38,6 +50,8 @@ public class UserController {
     
     @PutMapping("/me")
     @PreAuthorize("hasAnyRole('MANAGER', 'CUSTOMER')")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update user info")
     public UserResponseDto updateUserInfo(Authentication authentication,
                                       @RequestBody @Valid UpdateUserInfoRequestDto requestDto) {
         User user = (User) authentication.getPrincipal();
