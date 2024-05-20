33ecb5ecafbaa2274d6580ed71161360eaeced39
Nonzeq <bgdn.kobylchak@gmail.com>
2024-05-20 13:23:41 +0300

    added dto validation for payments

--- src/main/java/com/kobylchak/carsharing/dto/payment/CreatePaymentRequestDto.java
+++ src/main/java/com/kobylchak/carsharing/dto/payment/CreatePaymentRequestDto.java
@@ -1,10 +1,15 @@
 package com.kobylchak.carsharing.dto.payment;
 
 import com.kobylchak.carsharing.model.enums.PaymentType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class CreatePaymentRequestDto {
+    @NotNull
+    @Positive
     private Long rentalId;
+    @NotNull
     private PaymentType paymentType;
 }
--- src/main/java/com/kobylchak/carsharing/dto/rental/CreateRentalRequestDto.java
+++ src/main/java/com/kobylchak/carsharing/dto/rental/CreateRentalRequestDto.java
@@ -1,10 +1,16 @@
 package com.kobylchak.carsharing.dto.rental;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.time.LocalDate;
 import lombok.Data;
 
 @Data
 public class CreateRentalRequestDto {
+    @NotNull
+    @Positive
     private Long carId;
+    @NotNull
+    
     private LocalDate returnDate;
 }
